cd7d347b802a4b38aa75ed071929675cb5dc185e
iwalpola <iwalpola@gmail.com>
2016-03-01 12:36:44 +0530

    working forum

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome to the Free Citizen Society's online portal</div>
+                <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    Click <a href="forum">here</a> to go to the forum
+                    You are logged in!
                 </div>
             </div>
         </div>
